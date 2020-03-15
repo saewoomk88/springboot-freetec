6952cadc0484c4a75dd4732112dbab390f3f840c
saewoomk88 <43767934+saewoomk88@users.noreply.github.com>
2020-03-15 21:54:41 +0900

    네이버 로그인 추가, 시큐리티테스트 추가 ,, 변경 성공 223p

--- src/test/java/com/iu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/iu/book/springboot/web/IndexControllerTest.java
@@ -1,14 +1,14 @@
 package com.iu.book.springboot.web;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.boot.test.context.SpringBootTest;
+        import org.springframework.boot.test.web.client.TestRestTemplate;
+        import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+        import static org.assertj.core.api.Assertions.assertThat;
+        import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -17,14 +17,14 @@ public class IndexControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-     @Test
-     public void 메인페이지_로딩() throws Exception{
-         //given
-         //when
-         String body = this.restTemplate.getForObject("/",String.class);
+    @Test
+    public void 메인페이지_로딩() throws Exception{
+        //given
+        //when
+        String body = this.restTemplate.getForObject("/",String.class);
 
-         //then
-         assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
-     }
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+    }
 
 }
